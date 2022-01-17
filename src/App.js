9f23dd114d0f45907ee011fb4b3210537fcd91be
AlexeiLazar87 <AlexeiLazar87@gmail.com>
2022-01-17 14:47:39 +0200

    counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from 'react-redux';
+
+const Counter = () => {
+    const counter = useSelector((state) => state.counter)
+    const dispatch = useDispatch();
+    return (
+        <>
+        <h1>Counter: {counter}</h1>
+        <button onClick={() => {
+            dispatch({type: 'INC'})
+        }}>Increment</button>
+        <button onClick={() => {
+            dispatch({type: 'DEC'})
+        }}>Decrement</button>
+        <button onClick={() => {
+            dispatch({type: 'RESET'})
+        }}>Reset</button>
+        </>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Counter/>
+        </div>
+    );
 }
 
 export default App;
